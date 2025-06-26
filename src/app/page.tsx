999a055688c39a2b95da4f83d12ae2830dcd4761
gandres-digital <gandres.digital@gmail.com>
2025-06-26 12:43:47 +0200

    change placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,13 +31,13 @@ const FormularioPersonaje: FC = () => {
         <input {...register('name', { required: true, maxLength: 20 })} placeholder="Character name" className="p-2 border" />
         {errors.name && <span className="text-red-600">Field required</span>}
 
-        <input {...register('mainclass', { required: true, maxLength: 20 })} placeholder="Class" className="p-2 border" />
+        <input {...register('mainclass', { required: true, maxLength: 20 })} placeholder="Your class" className="p-2 border" />
         {errors.mainclass && <span className="text-red-600">Field required</span>}
 
-        <input {...register('talent', { required: true, maxLength: 20 })} placeholder="Talent" className="p-2 border" />
+        <input {...register('talent', { required: true, maxLength: 20 })} placeholder="Your talents" className="p-2 border" />
         {errors.talent && <span className="text-red-600">Field required</span>}
 
-        <input {...register('gs', { required: true, valueAsNumber: true, maxLength: 20 })} type="number" placeholder="GS" className="p-2 border" />
+        <input {...register('gs', { required: true, valueAsNumber: true, maxLength: 20 })} type="number" placeholder="GearScore" className="p-2 border" />
         {errors.gs && <span className="text-red-600">Field required</span>}
 
         <button type="submit" className="bg-blue-600 text-white px-4 py-2">Send</button>
